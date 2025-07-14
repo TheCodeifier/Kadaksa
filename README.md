bc8bfeb0922aed0f4b27c562900930d89b230ccc
Diomuzan <94070451+TheCodeifier@users.noreply.github.com>
2025-07-14 20:10:06 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Kadaksa
+Kadaska is a CLI based invoicing system which is written in Java. It lastly makes use of the Large Class & Duplicate Code code smell.
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+class InvoiceService {
+    public void invoiceProduct(String product) {
+        System.out.println("Start invoicing: " + product);
+        System.out.println("Invoice created for " + product);
+    }
+
+    public void invoiceService(String service) {
+        System.out.println("Start invoicing: " + service);
+        System.out.println("Invoice created for " + service);
+    }
+}
