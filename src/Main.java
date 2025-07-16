9e18fb3de5eafb3c5626f6f2a307372b4e015164
Diomuzan <94070451+TheCodeifier@users.noreply.github.com>
2025-07-16 13:36:12 +0200

    Deleted duplicate code

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
-class InvoiceService {
-    public void invoiceProduct(String product) {
+class FacturationHelper {
+    public void facturationHelper(String product) {
         System.out.println("Start invoicing: " + product);
         System.out.println("Invoice created for " + product);
-    }
-
-    public void invoiceService(String service) {
-        System.out.println("Start invoicing: " + service);
-        System.out.println("Invoice created for " + service);
     }
 }
