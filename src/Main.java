87a1e8a4aa8d69a855fa0c614b3268525085d26f
Diomuzan <94070451+TheCodeifier@users.noreply.github.com>
2025-07-16 16:30:22 +0200

    Finished with Kadaksa

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 class FacturationHelper {
-    public void facturationHelper(String product) {
-        System.out.println("Start invoicing: " + product);
-        System.out.println("Invoice created for " + product);
+    public static void processFacturation(String item) {
+        System.out.println("Start invoicing: " + item);
+        System.out.println("Invoice created for " + item);
+    }
+}
+
+class InvoicingService {
+    public void invoiceProduct(String product) {
+        FacturationHelper.processFacturation(product);
+    }
+
+    public void invoiceService(String service) {
+        FacturationHelper.processFacturation(service);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        InvoicingService service = new InvoicingService();
+        service.invoiceProduct("Laptop");
+        service.invoiceService("Maintenance contract");
     }
 }
